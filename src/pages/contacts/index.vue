321b8321622a9288f4700efdbbc60597ba33c5c6
xiaowei-one <meetxiaowei1109@163.com>
2023-02-20 20:28:09 +0800

    feat: catalogue

---
+++ src/pages/contacts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+    <text class="title">{{ title }}</text>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<style>
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
